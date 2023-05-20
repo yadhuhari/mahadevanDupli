9f2d5982a98ea406ebd1cbf7414605da2e9dafa3
PYRO_PRIEST!! <85893956+PYROPRIEST@users.noreply.github.com>
2023-05-20 21:36:06 +0530

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,35 @@
+#START AND SCRIPT
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>Am 𝑺𝒆𝒕𝒉𝒖𝒑𝒂𝒕𝒉𝒊⚡️....😉
+
+Only Made For @Pirate_cinemas_group
+
+Click Help For Solving Your Problem ..
+@Jackey_07
+</i>"""    
+    
+    HELP_TEXT = """
+Oh You Have Serious Problem and You need Urgent Help
+            
+@Jackey_07
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <a href=https://t.me/MYFILTER_ROBOT>𝑺𝒆𝒕𝒉𝒖𝒑𝒂𝒕𝒉𝒊⚡️</a>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/JACKEY_07">Jackey TG</a></i></b>
+
+<b>➥ Helper</b> : <b><i><a href="https://t.me/TheEvil_HK">ᴍʀ ᴇᴠɪʟ</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram V2 </a></i>
+
+<b>➥ Source Code</b> : <b> Sorry This Bot Is PVT </b>
+"""
+
+    STATUS_TEXT = """★ Total Files: {}
+★ Using Storage: {} MB
+★ Free Storage: {} MB"""
