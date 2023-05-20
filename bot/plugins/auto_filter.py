c8cb42c2dbfd6300ef329520c179721fa48fb3e7
PYRO_PRIEST!! <85893956+PYROPRIEST@users.noreply.github.com>
2023-05-20 21:44:42 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -18,10 +18,10 @@ ACTIVE_CHATS = {}
 db = Database()
 
 PICS = [
- "https://telegra.ph/file/fd2676c5859891d20a9b4.jpg",
- "https://telegra.ph/file/dd2b32c9330d239741a64.jpg",
- "https://telegra.ph/file/9c8684202600f671f5a67.jpg",
- "https://telegra.ph/file/958a33c7c16598eec03ce.jpg"
+ "https://telegra.ph/file/d944277dd394b3ea57dbc.jpg",
+ "https://telegra.ph/file/d944277dd394b3ea57dbc.jpg",
+ "https://telegra.ph/file/d944277dd394b3ea57dbc.jpg",
+ "https://telegra.ph/file/d944277dd394b3ea57dbc.jpg"
 ]
 
 @Bot.on_message(filters.text & filters.group, group=0)
