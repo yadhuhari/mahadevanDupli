7a073a843071a6e072501d1205858118153fdd7b
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-10 15:17:40 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -2,33 +2,15 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am 𝑺𝒆𝒕𝒉𝒖𝒑𝒂𝒕𝒉𝒊⚡️....😉
+    START_TEXT = """<b>Hello {}!!</b>
+<i>I Am Anjana You can Call Me as a Auto Filter V3 Bot...😉
 
-Only Made For @Pirate_cinemas_group
-
-Click Help For Solving Your Problem ..
-@Jackey_07
-</i>"""    
-    
-    HELP_TEXT = """
-Oh You Have Serious Problem and You need Urgent Help
-            
-@Jackey_07
-"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <a href=https://t.me/MYFILTER_ROBOT>𝑺𝒆𝒕𝒉𝒖𝒑𝒂𝒕𝒉𝒊⚡️</a>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/JACKEY_07">Jackey TG</a></i></b>
+Exclusively I am Made For Team Crazy Cinemas...!!
 
-<b>➥ Helper</b> : <b><i><a href="https://t.me/TheEvil_HK">ᴍʀ ᴇᴠɪʟ</a></i></b>
+🔷SUPPORT CHANNEL : @crazy_cinemas_official
 
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram V2 </a></i>
-
-<b>➥ Source Code</b> : <b> Sorry This Bot Is PVT </b>
-"""
+💠SUPPORT GROUP   : @crazy_cinemas_group
+</i>"""
 
     STATUS_TEXT = """★ Total Files: {}
 ★ Using Storage: {} MB
