1c52611258018eb99645ffd42e6e5ee05b26ee23
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-11 09:19:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -85,12 +85,12 @@ async def start(bot, update):
     )
     await update.reply_video(
         video=random.choice(VID),
-        caption=f"""**Hey {update.from_user.mention},
+        caption=f"""Hey {update.from_user.mention},
         
 Here is my kiss for my dear... 💋
 Go to Group for Movies...!!
 
-@crazy_cinemas_official**"""
+@crazy_cinemas_official"""
     )
      
      
