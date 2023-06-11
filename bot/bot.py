353e17bda6d82f7bfcc030f881eade67fba6f6c2
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-11 11:48:47 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -8,12 +8,7 @@ from aiohttp import web as webserver
 
 PORT_CODE = environ.get("PORT", "8080")
 
-from .user import User
-
 class Bot(Client):
-    USER: User = None
-    USER_ID: int = None
-
     def __init__(self):
         super().__init__(
             "bot",
@@ -26,25 +21,3 @@ class Bot(Client):
             bot_token=BOT_TOKEN,
             sleep_threshold=10
         )
-        self.LOGGER = LOGGER
-
-    async def start(self):
-        await super().start()
-        
-        client = webserver.AppRunner(await bot_run())
-        await client.setup()
-        bind_address = "0.0.0.0"
-        await webserver.TCPSite(client, bind_address,
-        PORT_CODE).start()
-        
-        bot_details = await self.get_me()
-        self.set_parse_mode(enums.ParseMode.HTML)
-        self.LOGGER(__name__).info(
-            f"@{bot_details.username}  started! "
-        )
-        self.USER, self.USER_ID = await User().start()
-        
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
