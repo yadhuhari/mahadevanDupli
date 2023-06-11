d1a814ec5a2384f0d7c2347de073dc59dd0579c5
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-11 11:45:06 +0530

    Update user.py

--- bot/user.py
+++ bot/user.py
@@ -15,15 +15,3 @@ class User(Client):
             api_id=API_ID,
             workers=20
         )
-        self.LOGGER = LOGGER
-
-    async def start(self):
-        await super().start()
-        try: await self.export_session_string()
-        except: pass
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
